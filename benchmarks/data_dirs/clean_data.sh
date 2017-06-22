4c57f659d2475524cfa612dbe373cdb911d90551
tgrogers <timrogers@gmail.com>
2017-06-22 13:35:49 -0400

    Fixing up the data fetching process

---
+++ benchmarks/data_dirs/clean_data.sh
@@ -0,0 +1,2 @@
+export DATA_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
+rm -fr $DATA_ROOT/cuda
--- benchmarks/data_dirs/get_data.sh
+++ benchmarks/data_dirs/get_data.sh
@@ -2,8 +2,7 @@ export DATA_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 # rodinia-2.0-ft data
 RODINIA_DIR=$DATA_ROOT/cuda/rodinia/2.0-ft
 if [ ! -d $RODINIA_DIR ]; then
-    mkdir -p $RODINIA_DIR
     wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/benchmark_data/rodinia-2.0-ft.tgz
-    tar xzvf rodinia.2.0-ft.tgz -C $RODINIA_DIR
-    rm rodinia.2.0-ft.tgz
+    tar xzvf rodinia-2.0-ft.tgz
+    rm rodinia-2.0-ft.tgz
 fi
