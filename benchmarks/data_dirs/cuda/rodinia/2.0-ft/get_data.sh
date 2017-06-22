d18585c482c9a41ecf0127d5df7cc1a47461f9f7
tgrogers <timrogers@gmail.com>
2017-06-21 22:40:55 -0400

    General script to pull the data files from a central location

---
+++ benchmarks/data_dirs/cuda/rodinia/2.0-ft/get_data.sh
@@ -0,0 +1,3 @@
+wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/benchmark_data/rodinia.2.0-ft.data.tgz
+tar xzvf rodinia.2.0-ft.data.tgz
+rm rodinia.2.0-ft.data.tgz
---
+++ benchmarks/data_dirs/get_data.sh
@@ -0,0 +1,9 @@
+export DATA_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
+# rodinia-2.0-ft data
+RODINIA_DIR=$DATA_ROOT/cuda/rodinia/2.0-ft
+if [ ! -d $RODINIA_DIR ]; then
+    mkdir -p $RODINIA_DIR
+    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/benchmark_data/rodinia.2.0-ft.data.tgz
+    tar xzvf rodinia.2.0-ft.data.tgz -C $RODINIA_DIR
+    rm rodinia.2.0-ft.data.tgz
+fi
