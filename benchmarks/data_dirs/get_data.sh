ed97bb7f860f78b9ac15ae5128818efa0b8ae178
tgrogers <timrogers@gmail.com>
2017-06-22 13:39:44 -0400

    forgot one last thing from the tar extract

--- benchmarks/data_dirs/get_data.sh
+++ benchmarks/data_dirs/get_data.sh
@@ -3,6 +3,6 @@ export DATA_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 RODINIA_DIR=$DATA_ROOT/cuda/rodinia/2.0-ft
 if [ ! -d $RODINIA_DIR ]; then
     wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/benchmark_data/rodinia-2.0-ft.tgz
-    tar xzvf rodinia-2.0-ft.tgz
+    tar xzvf rodinia-2.0-ft.tgz -C $DATA_ROOT
     rm rodinia-2.0-ft.tgz
 fi
