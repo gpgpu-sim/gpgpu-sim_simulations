f51ec32d5c82cb4464bbecca611112188936bc6c
tgrogers <tgrogers@purdue.edu>
2017-10-31 23:20:21 -0400

    have to explicitly fix 4.2...

--- benchmarks/src/cuda/common/common.mk
+++ benchmarks/src/cuda/common/common.mk
@@ -146,14 +146,14 @@ else
 endif
 
 # Compiler-specific flags (by default, we always use sm_10, sm_20, and sm_30), unless we use the SMVERSION template
-GENCODE_SM10 := -gencode=arch=compute_10,code=\"sm_10,compute_10\"
-GENCODE_SM13 := -gencode=arch=compute_13,code=\"sm_13,compute_13\"
-GENCODE_SM20 := -gencode=arch=compute_20,code=\"sm_20,compute_20\"
-GENCODE_SM30 := -gencode=arch=compute_30,code=\"sm_30,compute_30\"
-GENCODE_SM35 := -gencode=arch=compute_35,code=\"sm_35,compute_35\"
-GENCODE_SM50 := -gencode=arch=compute_50,code=\"sm_50,compute_50\"
-GENCODE_SM60 := -gencode=arch=compute_60,code=\"sm_60,compute_60\"
-GENCODE_SM62 := -gencode=arch=compute_62,code=\"sm_62,compute_62\"
+GENCODE_SM10 ?= -gencode=arch=compute_10,code=\"sm_10,compute_10\"
+GENCODE_SM13 ?= -gencode=arch=compute_13,code=\"sm_13,compute_13\"
+GENCODE_SM20 ?= -gencode=arch=compute_20,code=\"sm_20,compute_20\"
+GENCODE_SM30 ?= -gencode=arch=compute_30,code=\"sm_30,compute_30\"
+GENCODE_SM35 ?= -gencode=arch=compute_35,code=\"sm_35,compute_35\"
+GENCODE_SM50 ?= -gencode=arch=compute_50,code=\"sm_50,compute_50\"
+GENCODE_SM60 ?= -gencode=arch=compute_60,code=\"sm_60,compute_60\"
+GENCODE_SM62 ?= -gencode=arch=compute_62,code=\"sm_62,compute_62\"
 
 CXXFLAGS  += $(CXXWARN_FLAGS) $(CXX_ARCH_FLAGS)
 CFLAGS    += $(CWARN_FLAGS) $(CXX_ARCH_FLAGS)
