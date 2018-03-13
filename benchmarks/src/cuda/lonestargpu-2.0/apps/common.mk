1f33686507599ccffbdd8fea8056699228298aaf
tgrogers <tgrogers@purdue.edu>
2018-03-13 12:26:17 -0400

    Making the build process in line with our multi-cuda implementations

--- benchmarks/src/cuda/lonestargpu-2.0/apps/common.mk
+++ benchmarks/src/cuda/lonestargpu-2.0/apps/common.mk
@@ -6,12 +6,21 @@ GCC  		:= g++
 CC := $(GCC)
 CUB_DIR := $(TOPLEVEL)/../cub
 
-COMPUTECAPABILITY := sm_30
+# Compiler-specific flags (by default, we always use sm_10, sm_20, and sm_30), unless we use the SMVERSION template
+GENCODE_SM10 ?= -gencode=arch=compute_10,code=\"sm_10,compute_10\"
+GENCODE_SM13 ?= -gencode=arch=compute_13,code=\"sm_13,compute_13\"
+GENCODE_SM20 ?= -gencode=arch=compute_20,code=\"sm_20,compute_20\"
+GENCODE_SM30 ?= -gencode=arch=compute_30,code=\"sm_30,compute_30\"
+GENCODE_SM35 ?= -gencode=arch=compute_35,code=\"sm_35,compute_35\"
+GENCODE_SM50 ?= -gencode=arch=compute_50,code=\"sm_50,compute_50\"
+GENCODE_SM60 ?= -gencode=arch=compute_60,code=\"sm_60,compute_60\"
+GENCODE_SM62 ?= -gencode=arch=compute_62,code=\"sm_62,compute_62\"
+
 ifdef debug
-FLAGS := -arch=$(COMPUTECAPABILITY) -g -DLSGDEBUG=1 -G
+FLAGS := $(GENCODE_SM10)  $(GENCODE_SM13) $(GENCODE_SM20) $(GENCODE_SM30) $(GENCODE_SM35) $(GENCODE_SM35)  $(GENCODE_SM50) $(GENCODE_SM60)  $(GENCODE_SM62) -g -DLSGDEBUG=1 -G
 else
 # including -lineinfo -G causes launches to fail because of lack of resources, pity.
-FLAGS := -O3 -arch=$(COMPUTECAPABILITY) -g -Xptxas -v  #-lineinfo -G
+FLAGS := -O3 $(GENCODE_SM10)  $(GENCODE_SM13) $(GENCODE_SM20) $(GENCODE_SM30) $(GENCODE_SM35) $(GENCODE_SM35)  $(GENCODE_SM50) $(GENCODE_SM60)  $(GENCODE_SM62) -g -Xptxas -v  #-lineinfo -G
 endif
 INCLUDES := -I $(TOPLEVEL)/include -I $(CUB_DIR)
 LINKS := 
