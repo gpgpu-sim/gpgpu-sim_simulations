f338db42569ba7cfc7adee46c87c1349bd10b3e5
tgrogers <timrogers@purdue.edu>
2018-06-27 12:50:05 -0400

    Streamling the data get process

--- benchmarks/clean_data.sh
+++ benchmarks/clean_data.sh
@@ -1,7 +1,3 @@
 export DATA_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 DATA_ROOT+="/data_dirs"
-rm -fr $DATA_ROOT/cuda
-rm -fr $DATA_ROOT/pannotia
-rm -fr $DATA_ROOT/dragon
-rm -fr $DATA_ROOT/proxy-apps-doe
-rm -fr $DATA_ROOT/parboil
+rm -fr $DATA_ROOT/
